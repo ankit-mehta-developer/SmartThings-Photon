0b552f7ef67ffe793f94d5f4099c3c89f7794a8a
Ankit Mehta <ankit.mehta.me@gmail.com>
2016-03-26 02:42:18 +0800

    Text change
    Text change from "requested to switch on" to "requested to switch off"

--- PhotonSwitchDeviceHandler.groovy
+++ PhotonSwitchDeviceHandler.groovy
@@ -141,5 +141,5 @@ def updateStatus(status)
     if (status == "off") { sendEvent(name: "switch", value: "off", display: true, descriptionText: device.displayName + " was switched off") }
     if (status == "on") { sendEvent(name: "switch", value: "on", display: true, descriptionText: device.displayName + " was switched on") }
     if (status == "switchingOn") { sendEvent(name: "switch", value: "off", display: true, descriptionText: device.displayName + " requested to switch on") }
-    if (status == "switchingOff") { sendEvent(name: "switch", value: "on", display: true, descriptionText: device.displayName + " requested to switch on") }
+    if (status == "switchingOff") { sendEvent(name: "switch", value: "on", display: true, descriptionText: device.displayName + " requested to switch off") }
 }
